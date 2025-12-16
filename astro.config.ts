224f8404be6a81fd3f2768b63c721be1ab85d50c
BMO <meow@nermalcat69.dev>
2025-12-16 21:42:25 +0530

    sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
+
 export default defineConfig({
   site: 'https://arjunaditya.xyz',
   integrations: [mdx(), sitemap()],
