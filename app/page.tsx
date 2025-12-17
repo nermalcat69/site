65b625c6d04acb61a632f223ded8d5c22eb54dff
BMO <meow@nermalcat69.dev>
2025-12-17 17:29:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <p className="text-sm text-gray-600 dark:text-neutral-400 mt-1">
               Founder of
               <a
-                href="https://graycup.org"
+                href="https://graycup.in"
                 className="no-underline pl-1 font-medium hover:underline text-blue-700 dark:text-neutral-300"
               >
                 Gray Cup
@@ -85,8 +85,8 @@ export default function Home() {
           </div>
         </header>
         <p className="mt-6 mb-4 text-sm sm:text-base leading-relaxed dark:text-neutral-300">
-          I sell tea, matcha, and coffee on the internet involving as much as
-          software possible to make things seamless and sustainable.
+          I sell tea, matcha, and coffee with a long-term view on sustainability, supply chains, and people.<br />
+          I try to involve as much as softwares possible to make things seamless with as minimum carbon impact as possible.
         </p>
 
         <p className="mb-6 text-sm sm:text-base leading-relaxed dark:text-neutral-300">
@@ -103,8 +103,17 @@ export default function Home() {
           I still code as it's the biggest leverage in this age and a lot of businesses still don't understand the value of it. I try to use Open Source as much as possible.
         </p>
         <p className="mb-6 text-sm sm:text-base leading-relaxed dark:text-neutral-300">
-          I believe growth requires space and time. Constant stimulation erodes originality.
+          My hobbies involve designing, composting, running and observing how my body reacts.
         </p>
+        <a
+          className="inline-block mt-2"
+          href="https://cal.com/arjunaditya/30min?user=arjunaditya"
+          target="_blank"
+        >
+          <Button variant="lightgraybg" size="minor">
+            Schedule Call
+          </Button>
+        </a>
           <div className="mt-8 sm:mt-12 block grid grid-cols-1 md:grid-cols-3 gap-3.5">
             <Image
               src="/home/coconut.webp"
@@ -141,50 +150,6 @@ export default function Home() {
           .
         </p>
 
-        <a
-          className="inline-block mt-6 mb-2"
-          href="https://cal.com/arjunaditya/30min?user=arjunaditya"
-          target="_blank"
-        >
-          <Button variant="lightgraybg" size="minor">
-            Schedule Call
-          </Button>
-        </a>
-
-
-        <a
-          href="https://x.com/arjunaditya_"
-          className="no-underline  dark:text-white"
-        >
-          <svg
-            width="25"
-            height="25"
-            viewBox="0 0 20 20"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs></defs>
-            <g
-              id="Page-1"
-              stroke="none"
-              strokeWidth="1"
-              fill="none"
-              fillRule="evenodd"
-            >
-              <g
-                transform="translate(-140.000000, -7559.000000)"
-                fill="#000000"
-              >
-                <g id="icons" transform="translate(56.000000, 160.000000)">
-                  <path
-                    d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
-                    id="github-[#142]"
-                  ></path>
-                </g>
-              </g>
-            </g>
-          </svg>
-        </a>
       </main>
     </div>
   );
