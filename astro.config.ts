f3ab9fc7bdcc811d95a419b91132a6845ef53903
BMO <meow@nermalcat69.dev>
2025-12-17 13:39:03 +0530

    setup tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite";
 
 
 export default defineConfig({
   site: 'https://arjunaditya.xyz',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
+  vite: {plugins: [tailwindcss()]},
 });
