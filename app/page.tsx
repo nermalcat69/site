6a340c237122c7d11b203c9cc2e639bb2b933681
BMO <meow@nermalcat69.dev>
2025-12-17 17:17:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
                   d="M15.5 5H9.5C7.29086 5 5.5 6.79086 5.5 9V15C5.5 17.2091 7.29086 19 9.5 19H15.5C17.7091 19 19.5 17.2091 19.5 15V9C19.5 6.79086 17.7091 5 15.5 5Z"
                   strokeWidth="1.5"
                   strokeLinecap="round"
-                  className="stroke-black"
+                  className="stroke-black dark:stroke-white"
                   strokeLinejoin="round"
                 />
                 <path
@@ -56,7 +56,7 @@ export default function Home() {
                   clipRule="evenodd"
                   d="M12.5 15C10.8431 15 9.5 13.6569 9.5 12C9.5 10.3431 10.8431 9 12.5 9C14.1569 9 15.5 10.3431 15.5 12C15.5 12.7956 15.1839 13.5587 14.6213 14.1213C14.0587 14.6839 13.2956 15 12.5 15Z"
                   strokeWidth="1.5"
-                  className="stroke-black"
+                  className="stroke-black dark:stroke-white"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
@@ -67,7 +67,7 @@ export default function Home() {
                   height="2"
                   rx="1"
                   transform="rotate(-90 15.5 9)"
-                  className="fill-black"
+                  className="fill-black dark:fill-white"
                   fill="#000000"
                 />
                 <rect
@@ -77,7 +77,7 @@ export default function Home() {
                   height="1"
                   rx="0.5"
                   transform="rotate(-90 16 8.5)"
-                  className="stroke-black"
+                  className="stroke-black dark:stroke-white"
                   strokeLinecap="round"
                 />
               </svg>
