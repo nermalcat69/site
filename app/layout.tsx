34ae50f951fd9f9c49246ae0ffaad324c1e323ac
BMO <meow@nermalcat69.dev>
2025-12-17 17:07:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
-  title: "Arjun Aditya",
-  description: "Arjun Aditya's website",
+  title: "Arjun Aditya | Focusing on Sustainablity and Technology",
+  description: "Arjun Aditya is the founder/director of Gray Cup Enterprises, building sustainable tea, matcha, and coffee business. He has a background in software development and is passionate about using technology to make a positive impact on the environment.",
   keywords: ["Arjun Aditya", "Gray Cup", "Arjun Aditya's website", "Gray Cup's Founder", "Arjun Aditya's Tea", "Arjun Aditya's Matcha", "Arjun Aditya's Coffee"],
 };
 
