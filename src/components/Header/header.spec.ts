15d56e9738f850d09693cf4834fa3cd01825398f
Arjun Aditya <arjunaditya92006@gmail.com>
2026-03-09 09:38:46 +0530

    update

--- src/components/Header/header.spec.ts
+++ src/components/Header/header.spec.ts
@@ -5,7 +5,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("has title", async ({ page }) => {
-  await expect(page).toHaveTitle("Eva Decker");
+  await expect(page).toHaveTitle("Arjun Aditya");
 });
 
 test("changes theme", async ({ page }) => {
